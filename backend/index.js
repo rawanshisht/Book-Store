261b3bbf20cca7535610893bc6bff8303e55ada6
rawanshisht <rawan.shisht@gmail.com>
2024-07-26 11:57:31 +0100

    v1 done

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,21 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRoutes from "./routes/bookRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "PUT", "POST", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome");
